5934a13ea5db2801323e2c4967cdf6f6d5b00435
Cyril <cyril.silverman@tido-music.com>
2017-03-15 13:38:24 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const curr = require('curry');
+
+const isFunction = value => typeof value === 'function';
+
+const when = (predicate, transform, value) =>
+  (isFunction(predicate) && predicate(value)) || predicate
+    ? transform(value)
+    : value
+
+module.exports = curry(when);
